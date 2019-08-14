d2eca3f31752b32899f48b59bf6c1499d8df0410
Arthur Guiot <arguiot@gmail.com>
2019-08-14 18:29:15 +0200

    Created BroadcastJSNotification (aka Notification) object

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+class BroadcastJSNotification {
+	constructor(name, object) {
+		this.name = name
+		this.object = object
+	}
+}
 class NotificationCenter {
+	constructor() {
+		this.observers = {}
+	}
 	get default() {
 		if (typeof window.BroadcastJS_Shared_Instance == "undefined") {
 			window.BroadcastJS_Shared_Instance = new NotificationCenter()
@@ -7,17 +16,7 @@ class NotificationCenter {
 	}
 }
 
-// Browserify / Node.js
-if (typeof define === "function" && define.amd) {
-  define(() => new NotificationCenter());
-  // CommonJS and Node.js module support.
-} else if (typeof exports !== "undefined") {
-  // Support Node.js specific `module.exports` (which can be a function)
-  if (typeof module !== "undefined" && module.exports) {
-    exports = module.exports = new NotificationCenter();
-  }
-  // But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
-  exports.NotificationCenter = new NotificationCenter();
-} else if (typeof global !== "undefined") {
-  global.NotificationCenter = new NotificationCenter();
+export default {
+	Notification: BroadcastJSNotification,
+	NotificationCenter: NotificationCenter
 }
