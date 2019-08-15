4d8a4d3799d56253b45a79492940894fcbeca645
Arthur Guiot <arguiot@gmail.com>
2019-08-15 10:47:59 +0200

    Added global methods in NotificationCenter

--- index.js
+++ index.js
@@ -6,13 +6,31 @@ class BroadcastJSNotification {
 }
 class NotificationCenter {
 	constructor() {
-		this.observers = {}
+		this.observers = []
 	}
 	get default() {
 		if (typeof window.BroadcastJS_Shared_Instance == "undefined") {
 			window.BroadcastJS_Shared_Instance = new NotificationCenter()
 		}
 		return window.BroadcastJS_Shared_Instance
+	}
+	addObserver(name, callback, reference=null) {
+		this.observers.push([name, callback, object])
+	}
+	removeObserver(name, reference=null) {
+		this.observers.forEach((o, i) => {
+			if (o[0] == name && o[2] == reference) {
+				this.observers.splice(i, 1)
+			}
+		})
+	}
+	post(notification) {
+		const name = notification.name
+		this.observers.forEach((o, i) => {
+			if (o[0] == name) {
+				o[1](notification.object)
+			}
+		})
 	}
 }
 
