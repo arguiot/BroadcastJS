673ce7eecf4b3c79beabe91587ba3710fa12220c
Arthur Guiot <arguiot@gmail.com>
2020-12-14 13:25:30 -0500

    Added type definition

---
+++ broadcast.d.ts
@@ -0,0 +1,15 @@
+declare class BroadcastJSNotification {
+    name: string;
+    object: unknown;
+    constructor(name: string, object?: any);
+}
+declare class NotificationManager {
+    observers: Map<string, (object: unknown) => void>;
+    constructor();
+    get default(): any;
+    addObserver(name: string, callback: (object: unknown) => void, reference?: any): void;
+    removeObserver(name: string, reference?: any): void;
+    post(notification: BroadcastJSNotification): void;
+}
+export { BroadcastJSNotification as Notification };
+export declare const NotificationCenter: NotificationManager;
